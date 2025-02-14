5f92f5e44ba5e8189226902bb36b65c10fdd681f
jason <jasonlikenfs@gmail.com>
2025-02-14 16:24:32 +0800

    feat: updates, remove unnecessary posts

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,6 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'Jason Space';
+export const SITE_DESCRIPTION =
+  'Jason Space - Frontend and Full Stack Developer with extensive experience in modern web technologies, including React, Next.js, Vue.js, and Node.js.';
